4e439fe025fa202e232c4a6da8f339aab5a9dd69
Max Aubain <max.aubain@gmail.com>
2019-09-10 20:24:02 +0200

    Adds comments to label legacy code in the front end App.js, ToDoLists.jsx, and ToDoListForm.jsx

--- frontend/src/todos/components/ToDoListForm.jsx
+++ frontend/src/todos/components/ToDoListForm.jsx
@@ -1,99 +1,116 @@
-import React, { useState } from 'react'
-import { makeStyles } from '@material-ui/styles'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Button from '@material-ui/core/Button'
-import DeleteIcon from '@material-ui/icons/Delete'
-import AddIcon from '@material-ui/icons/Add'
-import Typography from '@material-ui/core/Typography'
-import { TextField } from '../../shared/FormFields'
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
+import Typography from "@material-ui/core/Typography";
+import { TextField } from "../../shared/FormFields";
 
 const useStyles = makeStyles({
   card: {
-    margin: '1rem'
+    margin: "1rem"
   },
   todoLine: {
-    display: 'flex',
-    alignItems: 'center'
+    display: "flex",
+    alignItems: "center"
   },
   textField: {
     flexGrow: 1
   },
   standardSpace: {
-    margin: '8px'
+    margin: "8px"
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
     flexGrow: 1
   }
-})
+});
 
 export const ToDoListForm = ({ toDoList, saveToDoList }) => {
-  const classes = useStyles()
-  const [todos, setTodos] = useState(toDoList.todos)
+  const classes = useStyles();
+  const [todos, setTodos] = useState(toDoList.todos);
 
   const handleSubmit = event => {
-    event.preventDefault()
-    saveToDoList(toDoList.id, { todos })
-  }
+    event.preventDefault();
+    saveToDoList(toDoList.id, { todos });  // saveToDoList() as defined in ToDoLists
+  };
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography variant='headline' component='h2'>
+        {/* ToDo list form label "First list", "Second list" */}
+        <Typography variant="headline" component="h2">
           {toDoList.title}
         </Typography>
+
         <form onSubmit={handleSubmit} className={classes.form}>
           {todos.map((name, index) => (
             <div key={index} className={classes.todoLine}>
-              <Typography className={classes.standardSpace} variant='title'>
+
+              {/* ToDo list item number */}
+              <Typography className={classes.standardSpace} variant="title">
                 {index + 1}
               </Typography>
+
+              {/* ToDo list input field */}
               <TextField
-                label='What to do?'
+                label="What to do?"
                 value={name}
                 onChange={event => {
-                  setTodos([ // immutable update
+                  setTodos([
+                    // immutable update
                     ...todos.slice(0, index),
                     event.target.value,
                     ...todos.slice(index + 1)
-                  ])
+                  ]);
                 }}
                 className={classes.textField}
               />
+
+              {/* Delete button */}
               <Button
-                size='small'
-                color='secondary'
+                size="small"
+                color="secondary"
                 className={classes.standardSpace}
                 onClick={() => {
-                  setTodos([ // immutable delete
+                  setTodos([
+                    // immutable delete
                     ...todos.slice(0, index),
                     ...todos.slice(index + 1)
-                  ])
+                  ]);
                 }}
               >
                 <DeleteIcon />
               </Button>
             </div>
           ))}
+
           <CardActions>
+
+            {/* Add ToDo button, on click add new empty ToDo form field */}
             <Button
-              type='button'
-              color='primary'
+              type="button"
+              color="primary"
               onClick={() => {
-                setTodos([...todos, ''])
+                setTodos([...todos, ""]);
               }}
             >
               Add Todo <AddIcon />
             </Button>
-            <Button type='submit' variant='contained' color='primary'>
+
+            {/* Save button, submits form info and triggers handleSubmit() */}
+            <Button type="submit" variant="contained" color="primary">
               Save
             </Button>
+
           </CardActions>
+
         </form>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
--- frontend/src/todos/components/ToDoLists.jsx
+++ frontend/src/todos/components/ToDoLists.jsx
@@ -1,74 +1,87 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ReceiptIcon from '@material-ui/icons/Receipt'
-import Typography from '@material-ui/core/Typography'
-import { ToDoListForm } from './ToDoListForm'
+import React, { Fragment, useState, useEffect } from "react";
+// useState https://reactjs.org/docs/hooks-state.html
+// useEffect https://reactjs.org/docs/hooks-effect.html
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ReceiptIcon from "@material-ui/icons/Receipt";
+import Typography from "@material-ui/core/Typography";
+import { ToDoListForm } from "./ToDoListForm";
+
+/* Define sleep function with argument in miliseconds using JS setTimeout()
+https://www.w3schools.com/jsref/met_win_settimeout.asp */
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const getPersonalTodos = () => {
-  return sleep(1000).then(() => Promise.resolve({
-    '0000000001': {
-      id: '0000000001',
-      title: 'First List',
-      todos: ['First todo of first list!']
-    },
-    '0000000002': {
-      id: '0000000002',
-      title: 'Second List',
-      todos: ['First todo of second list!']
-    }
-  }))
-}
+  return sleep(1000).then(() =>
+    /* "Hardcoded" todos upon startup. */
+    Promise.resolve({
+      "0000000001": {
+        id: "0000000001",
+        title: "First List",
+        todos: ["First todo of first list!"]
+      },
+      "0000000002": {
+        id: "0000000002",
+        title: "Second List",
+        todos: ["First todo of second list!"]
+      }
+    })
+  );
+};
 
 export const ToDoLists = ({ style }) => {
-  const [toDoLists, setToDoLists] = useState({})
-  const [activeList, setActiveList] = useState()
+  /* Instantiating the todo lists */
+  const [toDoLists, setToDoLists] = useState({});
+
+  /* Instantiating the activeList that appears when a todo list is clicked on */
+  const [activeList, setActiveList] = useState();
 
   useEffect(() => {
-    getPersonalTodos()
-      .then(setToDoLists)
-  }, [])
+    getPersonalTodos().then(setToDoLists);
+  }, []);
 
-  if (!Object.keys(toDoLists).length) return null
-  return <Fragment>
-    <Card style={style}>
-      <CardContent>
-        <Typography
-          variant='headline'
-          component='h2'
-        >
-          My ToDo Lists
-        </Typography>
-        <List>
-          {Object.keys(toDoLists).map((key) => <ListItem
-            key={key}
-            button
-            onClick={() => setActiveList(key)}
-          >
-            <ListItemIcon>
-              <ReceiptIcon />
-            </ListItemIcon>
-            <ListItemText primary={toDoLists[key].title} />
-          </ListItem>)}
-        </List>
-      </CardContent>
-    </Card>
-    {toDoLists[activeList] && <ToDoListForm
-      key={activeList} // use key to make React recreate component to reset internal state
-      toDoList={toDoLists[activeList]}
-      saveToDoList={(id, { todos }) => {
-        const listToUpdate = toDoLists[id]
-        setToDoLists({
-          ...toDoLists,
-          [id]: { ...listToUpdate, todos }
-        })
-      }}
-    />}
-  </Fragment>
-}
+  if (!Object.keys(toDoLists).length) return null;
+
+  return (
+    <Fragment>
+      {/* Top section with My ToDo Lists, and selection of First and Second List */}
+      <Card style={style}>
+        <CardContent>
+          <Typography variant="headline" component="h2">
+            My ToDo Lists
+          </Typography>
+          <List>
+            {Object.keys(toDoLists).map(key => (
+              <ListItem key={key} button onClick={() => setActiveList(key)}>
+                <ListItemIcon>
+                  <ReceiptIcon />
+                </ListItemIcon>
+                <ListItemText primary={toDoLists[key].title} />
+              </ListItem>
+            ))}
+          </List>
+        </CardContent>
+      </Card>
+
+      {/* ToDoList form which appears on clicking above list */}
+      {toDoLists[activeList] && (
+        <ToDoListForm
+          key={activeList} // use key to make React recreate component to reset internal state
+          toDoList={toDoLists[activeList]}
+          saveToDoList={(id, { todos }) => {
+            const listToUpdate = toDoLists[id];
+            setToDoLists({
+              ...toDoLists,
+              [id]: { ...listToUpdate, todos }
+            });
+          }}
+        />
+      )}
+    </Fragment>
+  );
+};
