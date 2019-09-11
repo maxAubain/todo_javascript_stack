a039ef3d1d85076215adcca4e882220daf80ed1e
Max Aubain <Max@Carmen-2-0.local>
2019-09-11 15:55:18 +0200

    Backend: Installs nodemon for auto-restart of server on code update, installs cors and updates index.js to use cors to allow frontend to make HTTP requests

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require("cors"); 
 
-const PORT = 3001
+const app = express();
+const PORT = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(cors({origin: 'http://localhost:3001'})); // Enables cross-origin HTTP requests (CORS) with white-listed client address.
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+const  getResponse = "Hello world"
+app.get("/", (req, res) => {
+  res.send(getResponse);
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
