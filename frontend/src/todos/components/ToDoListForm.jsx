6a9ace95a522a5e66264a933f0500c358cbc19f3
Max Aubain <maxaubain@protonmail.com>
2019-09-12 19:12:40 +0200

    Cleans comments from, and refactors functions order in, ToDoListForm.jsx

--- frontend/src/todos/components/ToDoListForm.jsx
+++ frontend/src/todos/components/ToDoListForm.jsx
@@ -30,28 +30,22 @@ const useStyles = makeStyles({
   }
 });
 
-let saved = false
-let autosaveTimerStart = false
+let saved = false;
+let autosaveTimerStart = false;
+let autosaveID;
+const TIMER = 1000;
 
 export const ToDoListForm = ({ toDoList, saveToDoList }) => {
   const classes = useStyles();
   const [todos, setTodos] = useState(toDoList.todos);
-  console.log("Are todos saved by render?", saved)
-  console.log("Has autosave timer initiated by render", autosaveTimerStart)
+  console.log("Are todos saved by render?", saved);
+  console.log("Has autosave timer initiated by render", autosaveTimerStart);
 
-  /* Prototype autosave function */
   const save = () => {
     saveToDoList(toDoList.id, { todos });
-    saved = true
-    console.log("Todos have saved", saved)
-  }
-
-  let autosaveID
-  if (saved === false && autosaveTimerStart === false) {
-    autosaveID = setTimeout(save, 1000);
-    autosaveTimerStart = true;
-    console.log("Autosave timer initiated with ID:", autosaveID)
-  }
+    saved = true;
+    console.log("Todos have saved", saved);
+  };
 
   const handleAutosaveReset = () => {
     if (autosaveTimerStart === true) {
@@ -59,12 +53,17 @@ export const ToDoListForm = ({ toDoList, saveToDoList }) => {
       autosaveTimerStart = false;
       clearTimeout(autosaveID);
     }
+  };
+
+  if (saved === false && autosaveTimerStart === false) {
+    autosaveID = setTimeout(save, TIMER);
+    autosaveTimerStart = true;
+    console.log("Autosave timer initiated with ID:", autosaveID);
   }
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        {/* ToDo list form label "First list", "Second list" */}
         <Typography variant="headline" component="h2">
           {toDoList.title}
         </Typography>
@@ -72,35 +71,28 @@ export const ToDoListForm = ({ toDoList, saveToDoList }) => {
         <form onChange={handleAutosaveReset} className={classes.form}>
           {todos.map((name, index) => (
             <div key={index} className={classes.todoLine}>
-              {/* ToDo list item number */}
               <Typography className={classes.standardSpace} variant="title">
                 {index + 1}
               </Typography>
-
-              {/* ToDo list input field */}
               <TextField
                 label="What to do?"
                 value={name}
                 onChange={event => {
                   setTodos([
-                    // immutable update
                     ...todos.slice(0, index),
-                    event.target.value, // inserts new todo at array location 'index'
+                    event.target.value,
                     ...todos.slice(index + 1)
                   ]);
                 }}
                 className={classes.textField}
               />
-
-              {/* Delete button */}
               <Button
                 size="small"
                 color="secondary"
                 className={classes.standardSpace}
                 onClick={() => {
                   setTodos([
-                    // immutable delete
-                    ...todos.slice(0, index), // deletes todo at array location 'index'
+                    ...todos.slice(0, index),
                     ...todos.slice(index + 1)
                   ]);
                   handleAutosaveReset();
@@ -110,9 +102,7 @@ export const ToDoListForm = ({ toDoList, saveToDoList }) => {
               </Button>
             </div>
           ))}
-
           <CardActions>
-            {/* Add ToDo button, on click add new empty ToDo form field */}
             <Button
               type="button"
               color="primary"
