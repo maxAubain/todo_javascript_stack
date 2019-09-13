6294e14fbee7b166f35ff2a3d3f1d83737943f93
Max Aubain <maxaubain@protonmail.com>
2019-09-13 20:39:08 +0200

    Adds acceptance test that verifies the finished state of todo is saved

--- frontend/cypress/integration/sandbox/userCanFinishToDo.spec.js
+++ frontend/cypress/integration/sandbox/userCanFinishToDo.spec.js
@@ -7,10 +7,12 @@ describe("User can finish todo list items", () => {
     cy.findByText("My ToDo Lists").should("exist");
   });
 
+  const index = 0; // Todo list item index
+  const TIMER = 1000;  // Autosave wait time
+
   it("User can finish todo list item by marking checkbox", () => {
     cy.findByText("First List").click();
 
-    const index = 0;
     cy.get("input")
       .eq(index)
       .then($inpt => {
@@ -21,5 +23,17 @@ describe("User can finish todo list items", () => {
           .check();
         expect($inpt).to.have.class("textFieldFinished");
       });
+  });
+
+  it("User can save finished todo list item", () => {
+    cy.get('[type="checkbox"]')
+      .eq(index)
+      .check();
+    cy.wait(TIMER);
+
+    cy.findByText("Second List").click();
+    cy.findByText("First List").click();
+
+    cy.get("input").eq(index).should("have.class", "textFieldFinished")
   });
 });
