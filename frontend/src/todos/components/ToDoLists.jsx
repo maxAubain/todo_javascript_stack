8aa24ad0e184dd8b055528a229a4b4fe0ef2ef2b
Max Aubain <maxaubain@protonmail.com>
2020-01-23 16:34:06 -0500

    ToDoLists: Refactors and adds comments to show data flow sequence more clearly, updates prodServerPath, refactors 'loading' gif location and render condition

--- frontend/src/todos/components/ToDoLists.jsx
+++ frontend/src/todos/components/ToDoLists.jsx
@@ -9,34 +9,50 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ReceiptIcon from "@material-ui/icons/Receipt";
 import Typography from "@material-ui/core/Typography";
 import { ToDoListForm } from "./ToDoListForm";
-import loading from "../../../src/img/loading.gif";
+import loadingGifFile from "../../../src/img/loading.gif";
 
 // const devServerPath = "http://localhost:3001/";
 const prodServerPath = "https://todo-list-is-here.herokuapp.com/";
-const postTodos = (serverPath, toDoLists) => axios.post(serverPath, toDoLists);
-
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-const getPersonalTodos = () => {
-  return sleep(1000).then(() =>
-    axios.get(prodServerPath).then(response => Promise.resolve(response.data))
-  );
-};
 
 export const ToDoLists = ({ style }) => {
+  // 1. Instantiate todo list data var and active list selection var.
   const [toDoLists, setToDoLists] = useState({});
   const [activeList, setActiveList] = useState();
 
+  // 2. Define: Post GET request to prodServerPath for todo list data.
+  const prodServerPath = "https://todo-list-js-server.herokuapp.com/";
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  const getPersonalTodos = () => {
+    return sleep(1000).then(() =>
+      axios.get(prodServerPath).then(response => Promise.resolve(response.data))
+    );
+  };
+
+  // 3. Send GET request.
   useEffect(() => {
     getPersonalTodos().then(setToDoLists);
   }, []);
 
-  if (!Object.keys(toDoLists).length) return null;
+  // 4. Define loading gif object.
+  const loadingGif = (
+    <img height="200px" width="300px" src={loadingGifFile}></img>
+  );
 
+  // 5. If GET request is not-yet successful, return loading gif.
+  if (!Object.keys(toDoLists).length)
+    return (
+      <Card style={style}>
+        <CardContent>{loadingGif}</CardContent>
+      </Card>
+    );
+
+  // 6. If ToDoLists state changes, post changes to server.
+  const postTodos = (serverPath, toDoLists) =>
+    axios.post(serverPath, toDoLists);
   postTodos(prodServerPath, toDoLists);
 
   return (
     <Fragment>
-      <img src={loading}></img>
       <Card style={style}>
         <CardContent>
           <Typography variant="headline" component="h2">
