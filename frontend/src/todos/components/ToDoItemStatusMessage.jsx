ce630c750878bc88d529b369f55832a074419181
Max Aubain <maxaubain@protonmail.com>
2020-01-23 19:55:16 -0500

    Fixes time difference calculation bug and adds styling for 'due today' and 'over due'

--- frontend/src/todos/components/ToDoItemStatusMessage.jsx
+++ frontend/src/todos/components/ToDoItemStatusMessage.jsx
@@ -24,38 +24,48 @@ const useStyles = makeStyles({
     textAlign: "center",
     color: "green"
   },
-  threeDaysRemaining: {
+  twoDaysRemaining: {
     marginLeft: "1rem",
     marginRight: "1rem",
     width: 150,
     textAlign: "center",
     color: "orange"
   },
-  overdue: {
+  today: {
     marginLeft: "1rem",
     marginRight: "1rem",
     width: 150,
     textAlign: "center",
     color: "red"
+  },
+  overdue: {
+    marginLeft: "1rem",
+    marginRight: "1rem",
+    width: 150,
+    textAlign: "center",
+    color: "purple"
   }
 });
 
 export const ToDoItemStatusMessage = ({ dueDate, finished }) => {
   const classes = useStyles();
-  const dueDateObj = new Date(dueDate);
+  var dueDateObj = new Date(dueDate);
+  dueDateObj.setHours(24); // Fix for new Date(dueDate) interpretting dueDate as GMT+0, not EST --> GMT-5
   const todaysDateObj = new Date();
   const msToDay = 1000 * 60 * 60 * 24;
-  const daysRemaining = Math.floor((dueDateObj - todaysDateObj) / msToDay);
+  const daysRemaining = Math.ceil((dueDateObj - todaysDateObj) / msToDay);
 
   const handleDaysRemaining = (daysRemaining, finished) => {
     if (finished === true) {
-      return "Item complete";
+      return "Item complete.";
     } else if (isNaN(daysRemaining)) {
-      return "No due date set";
-    } else if (daysRemaining >= 0) {
+      return "No due date set.";
+    } else if (daysRemaining >= 1) {
       return `Days remaining: ${daysRemaining}`;
+    } else if (daysRemaining === 0) {
+      return "Item is due today!";
     } else {
-      return "Overdue";
+      return "Overdue.";
     }
   };
 
@@ -64,10 +74,12 @@ export const ToDoItemStatusMessage = ({ dueDate, finished }) => {
       return classes.finished;
     } else if (isNaN(daysRemaining)) {
       return classes.undefined;
-    } else if (daysRemaining > 3) {
+    } else if (daysRemaining > 2) {
       return classes.manyDaysRemaining;
-    } else if (daysRemaining <= 3 && daysRemaining >= 0) {
-      return classes.threeDaysRemaining;
+    } else if (daysRemaining <= 2 && daysRemaining >= 1) {
+      return classes.twoDaysRemaining;
+    } else if (daysRemaining === 0) {
+      return classes.today;
     } else {
       return classes.overdue;
     }
