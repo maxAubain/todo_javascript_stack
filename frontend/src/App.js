f8890d55c29cfffa1c377bbe8da38aefdb8ebbd5
Max Aubain <maxaubain@protonmail.com>
2020-01-25 01:40:29 -0500

    Removes unused material-ui import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { ToDoLists } from "./todos/components/ToDoLists";
-import { Hidden } from "@material-ui/core";
 
 const toolbarStyle = {
   display: "flex",
